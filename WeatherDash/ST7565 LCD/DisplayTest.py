51767969d2c582eed5bb9df3e5697b77d098e5ac
Rcarballo2222 <rcarballo2222@gmail.com>
2018-05-04 07:21:33 +0000

    Added first weather icons and test images

--- WeatherDash/ST7565 LCD/DisplayTest.py
+++ WeatherDash/ST7565 LCD/DisplayTest.py
@@ -10,10 +10,15 @@ import st7565.lcd
 import st7565.ops
 import st7565.spidev
 
-
 lcd = st7565.lcd.LCD(adafruit=True)
+lcd.clear()
 screen = st7565.bitmap.Bitmap()
-image = images.create_image("r2d2.pbm")
-screen.drawbitmap(image, True, True)
+image = images.create_image("cloudy.pbm")
+image = image.resize((64, 64))
+screen.drawbitmap(image)
+screen.hscroll(60)
 lcd.write_buffer(screen)
+lcd.pos(4, 20)
+lcd.puts("79")
+
 
