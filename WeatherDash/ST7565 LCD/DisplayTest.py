56fa57328b97c6a5bc2c426fa3e49ef31e8c4c22
Rcarballo2222 <rcarballo2222@gmail.com>
2018-05-05 04:28:03 +0000

    - Renamed folder back to images as it seemed more appropriate - Made a create_images function that will auto create PIL objects for every image in the images directory

--- WeatherDash/ST7565 LCD/DisplayTest.py
+++ WeatherDash/ST7565 LCD/DisplayTest.py
@@ -3,7 +3,7 @@ from PIL import Image
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.SPI as SPI
 
-import st7565.icons.CreateImage as images
+import st7565.images.images as images
 import st7565.bitmap
 import st7565.fonts.font5x7 as font5x7
 import st7565.lcd
@@ -13,12 +13,13 @@ import st7565.spidev
 lcd = st7565.lcd.LCD(adafruit=True)
 lcd.clear()
 screen = st7565.bitmap.Bitmap()
-image = images.create_image("cloudy.pbm")
-image = image.resize((64, 64))
-screen.drawbitmap(image)
+
+
+weather_icons = images.create_images()
+sunny = weather_icons["sunny"].resize((64,64))
+screen.drawbitmap(sunny)
 screen.hscroll(60)
 lcd.write_buffer(screen)
 lcd.pos(4, 20)
 lcd.puts("79")
-
 
---
+++ WeatherDash/ST7565 LCD/st7565/images/images.py
@@ -0,0 +1,25 @@
+"""
+Indexes and lists images contained in this directory for use in bitmaps
+"""
+import os
+from os import walk
+from PIL import Image
+
+images_path = "/var/lib/cloud9/Git/Engi 301/WeatherDash/ST7565 LCD/st7565/images/"
+
+def create_images():
+    """
+    Creates a `PIL` image object for every image in this directory, callable in dictionary format. Images must end in `.pbm`
+    Using `images[filename]` will yield a `PIL` image object for that filename. 
+    """
+    images = {}
+    for (dirpath, dirnames, filenames) in walk(images_path):
+        break
+    for file in filenames:
+        if (".pbm" in file):
+            images[file[:-4]] = Image.open(images_path + file)
+    return images
+            
+
+
+
