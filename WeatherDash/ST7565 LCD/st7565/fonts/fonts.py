00f48b686da4730032b5bffbff3e9cec3b6cbd38
Rcarballo2222 <rcarballo2222@gmail.com>
2018-05-06 23:19:18 +0000

    Added multiple functions for fonts.py and images.py for ease of use

--- WeatherDash/ST7565 LCD/st7565/fonts/fonts.py
+++ WeatherDash/ST7565 LCD/st7565/fonts/fonts.py
@@ -1,11 +1,38 @@
 """
-Allows for creating, resizing, and using of fonts to create strings on lcd display
+Indexes and lists fonts contained in this directory for use in bitmaps. 
+Fonts must be in the form of an image containing 0-9 in ascending order with their regular letter spacing included.
 """
+from PIL import Image
+import st7565.images.images as images 
 
-#TODO: Create image for each glyph in font, create function that sends characters/strings to display
 
-def create_image(img_file):
+fonts_path = "/var/lib/cloud9/Git/Engi 301/WeatherDash/ST7565 LCD/st7565/fonts/"
+
+def create_font(font_name):
     """
-    Takes image file (ex: "r2d2.pbm") and returns an Image object for it. File must be placed in images folder
+    returns a dictionary containing a `PIL` image object for every number in the specified font image `font_name`.jpg, callable in dictionary format.
     """
-    return Image.open("/var/lib/cloud9/Git/Engi 301/WeatherDash/ST7565 LCD/st7565/images/" + img_file)
+    font = {}
+    try:
+        numbers = Image.open(fonts_path + font_name + ".jpg")
+        width, height = numbers.size
+        width = width/10
+        for i in range(9):
+            box = (i*(width), 0, (i + 1)*(width), height)
+            font[i] = numbers.crop() 
+    except IOError:
+        print("Specified font file: %s.jpg cannot be found" % font_name)
+    return font
+def display_c(c, font, screen, lcd, size, x=0, y=0):
+    """
+    Displays a character in the given `font` at the specified `x` and `y` coordinates
+    
+    `c`: A char
+    `font`: A font dictionary
+    `size`: An integer from 1-10, 10 being max size that can fit the display
+    """
+    char = font[c]
+    
+    images.display_img(c,screen,lcd,size,x,y)
+        
+    
--- WeatherDash/ST7565 LCD/st7565/images/images.py
+++ WeatherDash/ST7565 LCD/st7565/images/images.py
@@ -1,25 +1,59 @@
 """
-Indexes and lists images contained in this directory for use in bitmaps
+Allows for creation, manipulation, and displaying of images contained in this directory for use in bitmaps.
 """
 import os
 from os import walk
 from PIL import Image
 
+import st7565.bitmap
+import st7565.lcd
+
 images_path = "/var/lib/cloud9/Git/Engi 301/WeatherDash/ST7565 LCD/st7565/images/"
+
+def create_img(file_name):
+    """Returns a `PIL` image object of `file_name`. """
+    try:
+        return Image.open(images_path + file_name)
+    except IOError:
+        print("Specified image file: %s cannot be found" % file_name)
 
 def create_images():
     """
-    Creates a `PIL` image object for every image in this directory, callable in dictionary format. Images must end in `.pbm` or `.jpg`
-    Using `images[filename]` will yield a `PIL` image object for that filename. 
+    Returns a dictionary containing a `PIL` image object for every image in this directory. Images must end in `.pbm` or `.jpg`.
+    Using `images['filename']` will yield a `PIL` image object for that filename. 
     """
     images = {}
     for (dirpath, dirnames, filenames) in walk(images_path):
         break
     for file in filenames:
         if ((".pbm" in file) or (".jpg" in file)):
-            images[file[:-4]] = Image.open(images_path + file)
+            images[file[:-4]] = create_img(images_path + file)
     return images
-            
 
+def resize_image(img, size):
+    """
+    Returns a proportionally resized image to `size` percent of the original image.
+    
+    `img`: A `PIL` image object.
+    """
+    size /= 100
+    width, height = img.size
+    width = round(width*size)
+    height = round(height*size)
+    img.resize((width,height))
+    return img
+            
+def display_img(img, screen, lcd, size = 100, x=0, y=0):
+    """
+    Displays PIL image objects on the ST7565 display at a specified x and y coordinates.
+    
+    `img`: A `PIL` image object.
+    `screen`: A bitmap object. What will be written to the display.
+    `lcd`: An lcd object.
+    """
+    if not(size == 100):
+        img = resize_image(img)
+    screen.drawbitmap(img, x, y)
+    lcd.write_buffer(screen) 
 
 
